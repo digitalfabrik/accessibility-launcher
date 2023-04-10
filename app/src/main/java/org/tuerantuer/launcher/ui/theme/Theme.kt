4df5160329336d9fda6b56ace91b24e03f3a5718
bitpit <9284845+8bitpit@users.noreply.github.com>
2023-04-10 18:07:59 +0200

    :eyes: Theme: Use background color for status and navigation bars

--- app/src/main/java/org/tuerantuer/launcher/ui/theme/Theme.kt
+++ app/src/main/java/org/tuerantuer/launcher/ui/theme/Theme.kt
@@ -20,8 +20,6 @@ private val DarkColorScheme = darkColorScheme(
 
 private val LightColorScheme = lightColorScheme(
     primary = Color(0xFF6F37C3),
-    secondary = Color.Green,
-    tertiary = Color.Blue,
 
     primaryContainer = Color(0xFF121213),
     onPrimaryContainer = Color(0xFFF7F7F9),
@@ -30,38 +28,21 @@ private val LightColorScheme = lightColorScheme(
     background = Color(0xFFF7F7F9),
     onSurface = Color(0xFF121213),
     onBackground = Color(0xFF121213),
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun LauncherTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        else -> LightColorScheme
-//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//            val context = LocalContext.current
-//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//        }
-//        darkTheme -> DarkColorScheme
-//        else -> LightColorScheme
-    }
+    val colorScheme = LightColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            val backgroundColor = colorScheme.background.toArgb()
+            (view.context as Activity).window.statusBarColor = backgroundColor
+            (view.context as Activity).window.navigationBarColor = backgroundColor
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = true
         }
     }
 
