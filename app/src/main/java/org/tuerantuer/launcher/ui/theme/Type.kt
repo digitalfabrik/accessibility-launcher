49aea1a2ce80bed7d041e333fce7dd3e517bf028
bitpit <9284845+8bitpit@users.noreply.github.com>
2023-04-22 12:37:31 +0200

    :eyes: Typography: Enable hyphenation for all texts

--- app/src/main/java/org/tuerantuer/launcher/ui/theme/Type.kt
+++ app/src/main/java/org/tuerantuer/launcher/ui/theme/Type.kt
@@ -1,34 +1,27 @@
 package org.tuerantuer.launcher.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.Hyphens
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
-    ),
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+val Typography = run {
+    val defaultTypography = Typography()
+
+    // Enable hyphenation for all text styles. This makes the text more readable, especially large text.
+    Typography(
+        displayLarge = defaultTypography.displayLarge.copy(hyphens = Hyphens.Auto),
+        displayMedium = defaultTypography.displayMedium.copy(hyphens = Hyphens.Auto),
+        displaySmall = defaultTypography.displaySmall.copy(hyphens = Hyphens.Auto),
+        headlineLarge = defaultTypography.headlineLarge.copy(hyphens = Hyphens.Auto),
+        headlineMedium = defaultTypography.headlineMedium.copy(hyphens = Hyphens.Auto),
+        headlineSmall = defaultTypography.headlineSmall.copy(hyphens = Hyphens.Auto),
+        titleLarge = defaultTypography.titleLarge.copy(hyphens = Hyphens.Auto),
+        titleMedium = defaultTypography.titleMedium.copy(hyphens = Hyphens.Auto),
+        titleSmall = defaultTypography.titleSmall.copy(hyphens = Hyphens.Auto),
+        bodyLarge = defaultTypography.bodyLarge.copy(hyphens = Hyphens.Auto),
+        bodyMedium = defaultTypography.bodyMedium.copy(hyphens = Hyphens.Auto),
+        bodySmall = defaultTypography.bodySmall.copy(hyphens = Hyphens.Auto),
+        labelLarge = defaultTypography.labelLarge.copy(hyphens = Hyphens.Auto),
+        labelMedium = defaultTypography.labelMedium.copy(hyphens = Hyphens.Auto),
+        labelSmall = defaultTypography.labelSmall.copy(hyphens = Hyphens.Auto),
     )
-    */
-)
+}
