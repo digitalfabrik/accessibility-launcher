e2760437f7679d01765915e3913049b1b2f84ce8
tobi <tobi.3ska@gmail.com>
2024-04-11 16:13:53 +0200

    :construction: UI: Add scroll buttons to all apps screen and homescreen
    - Find out why the flickering occurs / remember doesn't prevent recomposition
    - make elements transparent when wallpaper is shown

--- app/src/main/java/org/tuerantuer/launcher/ui/screen/AllAppsScreen.kt
+++ app/src/main/java/org/tuerantuer/launcher/ui/screen/AllAppsScreen.kt
@@ -1,13 +1,24 @@
 package org.tuerantuer.launcher.ui.screen
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,6 +31,27 @@ import org.tuerantuer.launcher.ui.data.AppHomeScreenItem
 import org.tuerantuer.launcher.ui.data.ScreenState
 import org.tuerantuer.launcher.ui.data.UiState
 import org.tuerantuer.launcher.ui.theme.LauncherTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.ArrowUpward
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.tuerantuer.launcher.ui.components.ScrollScrimComponent
 
 /**
  * The screen that shows all installed apps.
@@ -29,31 +61,123 @@ fun AllAppsScreen(
     uiState: UiState,
     onOpenApp: (appItemInfo: AppItemInfo) -> Unit = {},
     onGoBack: () -> Unit = {},
+    coroutineScope: CoroutineScope,
 ) {
-    Column(
+    val gridState = rememberLazyGridState()
+    // this next line sets the initial scroll button state to false, meaning that the scroll buttons are not visible
+    val showScrollButtonState = remember { mutableStateOf(false) }
+
+    val scrollButtonsOpacity by animateFloatAsState (
+        targetValue = if (showScrollButtonState.value) 1f else 0f,
+        animationSpec = tween(durationMillis = 500, easing = LinearOutSlowInEasing),
+        label = "",
+    )
+
+    LaunchedEffect(key1 = uiState.allApps.size) {
+        val totalItemsCount = gridState.layoutInfo.totalItemsCount
+        val visibleItemsCount = gridState.layoutInfo.visibleItemsInfo.size
+        val shouldShow = totalItemsCount > visibleItemsCount
+        if (shouldShow != showScrollButtonState.value) {
+            showScrollButtonState.value = shouldShow
+        }
+    }
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(LauncherTheme.all.onWallpaperBackground),
+            .background(LauncherTheme.all.onWallpaperBackground), //todo only use this when there is no wallpaper?
     ) {
-        HeaderComponent(
-            text = stringResource(R.string.all_apps),
-            onGoBack = onGoBack,
-        )
-        val homeScreenItems =
-            uiState.allApps.map { appItemInfo -> AppHomeScreenItem(appItemInfo, onClick = { onOpenApp(appItemInfo) }) }
-        val appIconSize = uiState.settings.appIconSize.sizeDp.dp
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = appIconSize),
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
         ) {
-            items(
-                items = homeScreenItems,
-                key = { homeScreenItems -> homeScreenItems.key },
-            ) { homeScreenItem ->
-                HomeScreenItemComponent(
-                    homeScreenItem = homeScreenItem,
-                    iconSize = appIconSize,
+            HeaderComponent(
+                text = stringResource(R.string.all_apps),
+                onGoBack = onGoBack,
+            )
+            val homeScreenItems =
+                uiState.allApps.map { appItemInfo ->
+                    AppHomeScreenItem(
+                        appItemInfo,
+                        onClick = { onOpenApp(appItemInfo) })
+                }
+            val appIconSize = uiState.settings.appIconSize.sizeDp.dp
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+            ) {
+                LazyVerticalGrid(
+                    state = gridState,
+                    columns = GridCells.Adaptive(minSize = appIconSize),
+                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                ) {
+                    items(
+                        items = homeScreenItems,
+                        key = { homeScreenItems -> homeScreenItems.key },
+                    ) { homeScreenItem ->
+                        HomeScreenItemComponent(
+                            homeScreenItem = homeScreenItem,
+                            iconSize = appIconSize,
+                        )
+                    }
+                }
+                ScrollScrimComponent(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .alpha(scrollButtonsOpacity)
+//                        .background(
+//                            brush = Brush.verticalGradient(
+//                                colors = listOf(
+//                                    Color.Transparent, // This makes the start of the gradient fully transparent
+//                                    Color.Transparent,
+//                                ),
+//                            ),
+//                        ),
+
                 )
+            }
+            if (showScrollButtonState.value) {
+                Row(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+//                        .background(Color(0f, 0f, 0f, 0.1f)) // todo only use this when there is no wallpaper?
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(vertical = 16.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+
+                    ) {
+                    val scrollDistance = 1000.dp.value
+                    FloatingActionButton(onClick = {
+                        coroutineScope.launch {
+                            gridState.animateScrollBy(
+                                -scrollDistance,
+                                tween(durationMillis = 500, easing = LinearOutSlowInEasing)
+                            )
+                        }
+                    }) {
+                        Icon(
+                            Icons.Filled.ArrowUpward,
+                            contentDescription = stringResource(id = R.string.button_scroll_up),
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(50.dp))
+                    FloatingActionButton(onClick = {
+                        coroutineScope.launch {
+                            gridState.animateScrollBy(
+                                value = scrollDistance,
+                                animationSpec = tween(durationMillis = 500, easing = LinearOutSlowInEasing)
+                            )
+                        }
+                    }) {
+                        Icon(
+                            Icons.Filled.ArrowDownward,
+                            contentDescription = stringResource(id = R.string.button_scroll_down),
+                        )
+                    }
+                }
             }
         }
     }
@@ -67,6 +191,7 @@ fun AllAppsScreenPreview() {
     LauncherTheme {
         AllAppsScreen(
             uiState = UiState(ScreenState.AllAppsScreenState),
+            coroutineScope = CoroutineScope(GlobalScope.coroutineContext), // todo change?
         )
     }
 }
--- app/src/main/java/org/tuerantuer/launcher/ui/screen/HomeScreen.kt
+++ app/src/main/java/org/tuerantuer/launcher/ui/screen/HomeScreen.kt
@@ -2,30 +2,55 @@ package org.tuerantuer.launcher.ui.screen
 
 import android.text.format.DateFormat
 import android.widget.TextClock
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.ArrowUpward
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.tuerantuer.launcher.R
 import org.tuerantuer.launcher.app.AppItemInfo
 import org.tuerantuer.launcher.ui.components.HomeScreenItemComponent
+import org.tuerantuer.launcher.ui.components.ScrollScrimComponent
 import org.tuerantuer.launcher.ui.data.AppHomeScreenItem
 import org.tuerantuer.launcher.ui.data.ButtonHomeScreenItem
 import org.tuerantuer.launcher.ui.data.HomeScreenItem
@@ -33,6 +58,7 @@ import org.tuerantuer.launcher.ui.data.ScreenState
 import org.tuerantuer.launcher.ui.data.UiState
 import org.tuerantuer.launcher.ui.theme.LauncherTheme
 import org.tuerantuer.launcher.util.extension.setShadow
+import timber.log.Timber
 import java.util.Locale
 
 private const val DATE_PATTERN = "EEEE, MMMd"
@@ -40,6 +66,7 @@ private const val TIME_PATTERN = "HH:mm"
 
 /**
  * The main screen of the launcher. Here, the user can see the clock, the favorites and can access other screens.
+ * If the grid content exceeds the visible screen space, scroll buttons are shown.
  */
 @Composable
 fun HomeScreen(
@@ -49,66 +76,146 @@ fun HomeScreen(
     onEditFavorites: () -> Unit,
     onShowOnboarding: () -> Unit,
     onOpenApp: (appItemInfo: AppItemInfo) -> Unit,
+    gridState: LazyGridState,
+    coroutineScope: CoroutineScope,
 ) {
+    val showScrollButtonState = remember {
+        Timber.d("Carikom: remember: showScrollButtonState")
+        mutableStateOf(false)
+    } // false
+    val scrollButtonsOpacity by animateFloatAsState (
+        targetValue = if (showScrollButtonState.value) 1f else 0f,
+        animationSpec = tween(durationMillis = 500, easing = LinearOutSlowInEasing),
+        label = "",
+    )
+    // todo hallo zukunfts ich, warum 2 compositionen? jetzt dein problem
+    Timber.d("Carikom: showScrollButtonState: ${showScrollButtonState.value}")
+    LaunchedEffect(key1 = uiState.allApps.size) {
+        Timber.d("Carikom: LaunchedEffect: uiState.allApps.size: ${uiState.allApps.size}" )
+        val totalItemsCount = gridState.layoutInfo.totalItemsCount
+        Timber.d("Carikom: totalItemsCount: $totalItemsCount" )
+        val visibleItemsCount = gridState.layoutInfo.visibleItemsInfo.size
+        Timber.d("Carikom: visibleItemsCount: $visibleItemsCount" )
+        val shouldShow = totalItemsCount > visibleItemsCount
+        if (shouldShow != showScrollButtonState.value) { // as long as there are less items than visible, this will not be called
+//            showScrollButtonState.value = shouldShow
+            Timber.d("Carikom: updated state: ${showScrollButtonState.value}")
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(LauncherTheme.all.onWallpaperBackground),
     ) {
-        val homeScreenItems = mutableListOf<HomeScreenItem>().apply {
-            uiState.favorites.mapTo(this) { appItemInfo ->
-                AppHomeScreenItem(appItemInfo, onClick = { onOpenApp(appItemInfo) })
+        Box(
+            modifier = Modifier
+                .weight(1f),
+        ) {
+            val homeScreenItems = mutableListOf<HomeScreenItem>().apply {
+                uiState.favorites.mapTo(this) { appItemInfo ->
+                    AppHomeScreenItem(appItemInfo, onClick = { onOpenApp(appItemInfo) })
+                }
+                val context = LocalContext.current
+                add(
+                    ButtonHomeScreenItem(
+                        nameRes = R.string.all_apps,
+                        innerIconRes = R.drawable.baseline_apps_24,
+                        context = context,
+                        onClick = onShowAllApps,
+                    ),
+                )
+                add(
+                    ButtonHomeScreenItem(
+                        nameRes = R.string.change_favorites,
+                        innerIconRes = R.drawable.outline_interests_24,
+                        context = context,
+                        onClick = onEditFavorites,
+                    ),
+                )
+                add(
+                    ButtonHomeScreenItem(
+                        nameRes = R.string.setup_assistant,
+                        innerIconRes = R.drawable.outline_help_outline_24,
+                        context = context,
+                        onClick = onShowOnboarding,
+                    ),
+                )
+                add(
+                    ButtonHomeScreenItem(
+                        nameRes = R.string.settings,
+                        innerIconRes = R.drawable.outline_settings_24,
+                        context = context,
+                        onClick = onOpenSettings,
+                    ),
+                )
             }
-            val context = LocalContext.current
-            add(
-                ButtonHomeScreenItem(
-                    nameRes = R.string.all_apps,
-                    innerIconRes = R.drawable.baseline_apps_24,
-                    context = context,
-                    onClick = onShowAllApps,
-                ),
-            )
-            add(
-                ButtonHomeScreenItem(
-                    nameRes = R.string.change_favorites,
-                    innerIconRes = R.drawable.outline_interests_24,
-                    context = context,
-                    onClick = onEditFavorites,
-                ),
-            )
-            add(
-                ButtonHomeScreenItem(
-                    nameRes = R.string.setup_assistant,
-                    innerIconRes = R.drawable.outline_help_outline_24,
-                    context = context,
-                    onClick = onShowOnboarding,
-                ),
-            )
-            add(
-                ButtonHomeScreenItem(
-                    nameRes = R.string.settings,
-                    innerIconRes = R.drawable.outline_settings_24,
-                    context = context,
-                    onClick = onOpenSettings,
-                ),
+            val appIconSize = uiState.settings.appIconSize.sizeDp.dp
+            LazyVerticalGrid(
+                state = gridState,
+                columns = GridCells.Adaptive(minSize = appIconSize),
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            ) {
+                item(span = { GridItemSpan(Int.MAX_VALUE) }) {
+                    Clock()
+                }
+                items(
+                    items = homeScreenItems,
+                    key = { homeScreenItem -> homeScreenItem.key },
+                ) { homeScreenItem ->
+                    HomeScreenItemComponent(
+                        homeScreenItem = homeScreenItem,
+                        iconSize = appIconSize,
+                    )
+                }
+            }
+
+            ScrollScrimComponent(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .alpha(scrollButtonsOpacity),
             )
         }
-        val appIconSize = uiState.settings.appIconSize.sizeDp.dp
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = appIconSize),
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        ) {
-            item(span = { GridItemSpan(Int.MAX_VALUE) }) {
-                Clock()
-            }
-            items(
-                items = homeScreenItems,
-                key = { homeScreenItem -> homeScreenItem.key },
-            ) { homeScreenItem ->
-                HomeScreenItemComponent(
-                    homeScreenItem = homeScreenItem,
-                    iconSize = appIconSize,
-                )
+
+        if (showScrollButtonState.value) {
+            Row(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.background) // todo
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(vertical = 16.dp)
+                    .alpha(scrollButtonsOpacity),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                val scrollDistance = 1000.dp.value
+                FloatingActionButton(onClick = {
+                    coroutineScope.launch {
+                        gridState.animateScrollBy(
+                            -scrollDistance,
+                            tween(durationMillis = 500, easing = LinearOutSlowInEasing)
+                        )
+                    }
+                }) {
+                    Icon(
+                        Icons.Filled.ArrowUpward,
+                        contentDescription = "up",
+                    )
+                }
+                Spacer(modifier = Modifier.width(50.dp))
+                FloatingActionButton(onClick = {
+                    coroutineScope.launch {
+                        gridState.animateScrollBy(
+                            value = scrollDistance,
+                            animationSpec = tween(durationMillis = 500, easing = LinearOutSlowInEasing)
+                        )
+                    }
+                }) {
+                    Icon(
+                        Icons.Filled.ArrowDownward,
+                        contentDescription = "down",
+                    )
+                }
             }
         }
     }
@@ -187,6 +294,8 @@ fun HomeScreenPreview() {
             onEditFavorites = { },
             onShowOnboarding = { },
             onOpenApp = { },
+            gridState = LazyGridState(),
+            coroutineScope = CoroutineScope(GlobalScope.coroutineContext),
         )
     }
 }
